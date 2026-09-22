4ba22e2d6e983a0dcbd494e60fd17ba86de82ec1
CHRISTIAN BAUREL SUMBANG <tsumbang@outlook.com>
2026-09-22 13:55:27 -0400

    test: expect teleconsultation migration V11

--- src/test/java/org/novasos/healthysv2/DatabaseMigrationIntegrationTests.java
+++ src/test/java/org/novasos/healthysv2/DatabaseMigrationIntegrationTests.java
@@ -69,7 +69,7 @@ class DatabaseMigrationIntegrationTests {
                 """,
                 String.class);
 
-        assertThat(versions).containsExactly("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
+        assertThat(versions).containsExactly("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11");
     }
 
     @Test
