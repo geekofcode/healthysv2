17cb0e9100003ff389c6aba4913a2cb0a7a47dc4
CHRISTIAN BAUREL SUMBANG <tsumbang@outlook.com>
2026-09-26 09:01:09 -0400

    test: fix billing API fixture import

--- src/test/java/org/novasos/healthysv2/billing/BillingApiIntegrationTests.java
+++ src/test/java/org/novasos/healthysv2/billing/BillingApiIntegrationTests.java
@@ -9,7 +9,7 @@ import java.util.*;
 import org.junit.jupiter.api.Test;
 import org.novasos.healthysv2.TestcontainersConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.*;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.context.annotation.*;
 import org.springframework.http.*;
